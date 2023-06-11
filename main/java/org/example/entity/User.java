b6db0892634b6dcbac8987a3c9aa555af805baa6
armin <arminjamali73@gmail.com>
2023-06-11 16:18:32 +0330

    create user class

---
+++ main/java/org/example/entity/User.java
@@ -0,0 +1,18 @@
+package org.example.entity;
+
+public class User {
+    private  int id ;
+    private  String name ;
+    private  String username ;
+    private  String email ;
+    private  String password ;
+
+    public User(int id, String name, String username, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+}
