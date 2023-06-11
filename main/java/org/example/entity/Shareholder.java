d3d49f229040834ca148689494bdd15ea0e6972a
armin <arminjamali73@gmail.com>
2023-06-11 16:02:55 +0330

    Creating class and related method and constructor without ID and creating setter and getter

--- main/java/org/example/entity/Shareholder.java
+++ main/java/org/example/entity/Shareholder.java
@@ -13,4 +13,41 @@ public class Shareholder {
         this.nationalCode = nationalCode;
     }
 
+    public Shareholder(String name, int phoneNumber, int nationalCode) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(int nationalCode) {
+        this.nationalCode = nationalCode;
+    }
 }
