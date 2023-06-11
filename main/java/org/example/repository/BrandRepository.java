55b486a6d3e6012954828d88aa3139f9b03df8f6
armin <arminjamali73@gmail.com>
2023-06-11 17:21:06 +0330

    Repository brand and create save to record in the table and load to remove and delete to delete

---
+++ main/java/org/example/repository/BrandRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.entity.Brand;
+
+import java.sql.SQLException;
+
+public interface BrandRepository {
+    void save (Brand brand) throws SQLException;
+    Brand load (int brandId) throws SQLException;
+    void delete (int brandId) throws SQLException;
+
+}
