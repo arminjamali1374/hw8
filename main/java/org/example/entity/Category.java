f1fe592b8bf80aef3e9af2ada5786f5893fde1fb
armin <arminjamali73@gmail.com>
2023-06-11 15:12:06 +0330

    created class Category

---
+++ main/java/org/example/entity/Category.java
@@ -0,0 +1,15 @@
+package org.example.entity;
+
+public class Category {
+    private  int id ;
+    private  String name ;
+    private String description ;
+
+
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+}
