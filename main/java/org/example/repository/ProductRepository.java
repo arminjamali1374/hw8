4c05f85795a83a3201b5347f4c1b1e463a6c3975
armin <arminjamali73@gmail.com>
2023-06-11 18:29:48 +0330

    Product repository and create save to record in the table and load to remove and delete to delete

---
+++ main/java/org/example/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.entity.Category;
+import org.example.entity.Product;
+
+import java.sql.SQLException;
+
+public interface ProductRepository {
+    void save (Product product) throws SQLException;
+    Product load (int productId) throws SQLException;
+    void delete (int productId) throws SQLException;
+}
