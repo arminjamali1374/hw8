82333f4163ca7a1955b6cbb9fc75cdd404a60959
armin <arminjamali73@gmail.com>
2023-06-11 14:56:54 +0330

    created class brand

---
+++ main/java/org/example/entity/Brand.java
@@ -0,0 +1,16 @@
+package org.example.entity;
+
+public class Brand {
+    private  int id ;
+    private  String name ;
+    private  String website ;
+    private  String description ;
+
+    public Brand(int id, String name, String website, String description) {
+        this.id = id;
+        this.name = name;
+        this.website = website;
+        this.description = description;
+    }
+
+}
