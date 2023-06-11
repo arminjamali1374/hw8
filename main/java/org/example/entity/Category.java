134efe5484aca984020e2791511c9e1e7f576f52
armin <arminjamali73@gmail.com>
2023-06-11 15:17:00 +0330

    created method and contractor class Category

--- main/java/org/example/entity/Category.java
+++ main/java/org/example/entity/Category.java
@@ -12,4 +12,33 @@ public class Category {
         this.description = description;
     }
 
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
