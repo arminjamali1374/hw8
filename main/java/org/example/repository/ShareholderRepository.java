29e4f243db6e8361323ec8f22767c198d6c62025
armin <arminjamali73@gmail.com>
2023-06-12 10:16:35 +0330

    Shareholder repository and create save to record in the table and load to remove and delete to delete

---
+++ main/java/org/example/repository/ShareholderRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.entity.Category;
+import org.example.entity.Shareholder;
+
+import java.sql.SQLException;
+
+public interface ShareholderRepository {
+    void save (ShareholderRepository shareholderRepository) throws SQLException;
+    Shareholder load (int ShareholderId) throws SQLException;
+    void delete (int ShareholderId) throws SQLException;
+}
