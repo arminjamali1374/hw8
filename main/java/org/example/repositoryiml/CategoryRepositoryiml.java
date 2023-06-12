e9ef1cea97543d8257f1c215416624e3ac969859
armin <arminjamali73@gmail.com>
2023-06-12 17:12:11 +0330

    created method save and load and delete writer field method

---
+++ main/java/org/example/repositoryiml/CategoryRepositoryiml.java
@@ -0,0 +1,46 @@
+package org.example.repositoryiml;
+
+import org.example.connection.ApplicationContext;
+import org.example.entity.Category;
+import org.example.repository.CategoryRepository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoryRepositoryiml implements CategoryRepository {
+    @Override
+    public void save(Category category) throws SQLException {
+        String query = """
+                INSERT INTO "category" (name, description) values (?,?)
+                """;
+        PreparedStatement preparedStatement = ApplicationContext.getConnection().prepareStatement(query);
+        preparedStatement.setString(1,category.getName());
+        preparedStatement.setString(2,category.getDescription());
+        preparedStatement.execute();
+
+    }
+
+    @Override
+    public Category load(int categoryId) throws SQLException {
+        String query = """
+                SELECT * FROM "category" WHERE id = ?
+                """;
+        PreparedStatement preparedStatement = ApplicationContext.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1,categoryId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        return new Category(categoryId,resultSet.getString(2),resultSet.getString(3));
+    }
+
+    @Override
+    public void delete(int categoryId) throws SQLException {
+        String query = """
+                DELETE FROM category WHERE id = ?
+                """;
+        PreparedStatement preparedStatement = ApplicationContext.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1,categoryId);
+        preparedStatement.execute();
+    }
+}
