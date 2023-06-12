61d985990e5b77af22ab9d43228b1917e758a59d
armin <arminjamali73@gmail.com>
2023-06-12 17:32:31 +0330

    Write save, delete and load for brand in product class

---
+++ main/java/org/example/repositoryiml/ProductRepositoryiml.java
@@ -0,0 +1,47 @@
+package org.example.repositoryiml;
+
+import org.example.connection.ApplicationContext;
+import org.example.entity.Product;
+import org.example.repository.ProductRepository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductRepositoryiml implements ProductRepository {
+    @Override
+    public void save(Product product) throws SQLException {
+        String query= """
+                insert into "product" (name,create_date,category_id,brand_id) values (?,?,?,?)
+                """;
+        PreparedStatement preparedStatement= ApplicationContext.getConnection().prepareStatement(query);
+        preparedStatement.setString(1,product.getName());
+        preparedStatement.setString(2,product.getCreateData());
+        preparedStatement.setInt(3,product.getCategoryId());
+        preparedStatement.setInt(4,product.getBrandId());
+        preparedStatement.execute();
+    }
+
+    @Override
+    public Product load(int productId) throws SQLException {
+        String query= """
+                select * FROM "product"  where id=?
+                """;
+        PreparedStatement preparedStatement=ApplicationContext.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1,productId);
+        ResultSet resultSet= preparedStatement.executeQuery();
+        resultSet.next();
+        return new Product(productId,resultSet.getString(2),resultSet.getString(3),resultSet.getInt(4),resultSet.getInt(5));
+    }
+
+    @Override
+    public void delete(int productId) throws SQLException {
+        String query= """
+                delete from "product" where id=?
+                """;
+        PreparedStatement preparedStatement=ApplicationContext.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1,productId);
+        preparedStatement.execute();
+
+    }
+}
