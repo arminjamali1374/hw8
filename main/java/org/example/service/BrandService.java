6bb6d33ebf5cb1829fbca442ba92df05038549a8
armin <arminjamali73@gmail.com>
2023-06-14 11:04:30 +0330

    Load, save and delete were placed in the service

---
+++ main/java/org/example/service/BrandService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.example.entity.Brand;
+
+import java.sql.SQLException;
+
+public interface BrandService {
+    void save(Brand brand) throws SQLException;
+    Brand load(int brandId) throws SQLException;
+    void delete(int brandId) throws SQLException;
+}
