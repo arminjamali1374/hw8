0660eb56a04050dd5729490c30bef77b7bc75de0
armin <arminjamali73@gmail.com>
2023-06-14 11:06:04 +0330

    Load, save and delete were placed in the service

---
+++ main/java/org/example/service/ShareholderService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import org.example.entity.Shareholder;
+import org.example.entity.User;
+
+import java.sql.SQLException;
+
+public interface ShareholderService {
+    void save(Shareholder shareholder) throws SQLException;
+    Shareholder load(int ShareholderId) throws SQLException;
+    void delete(int ShareholderId) throws SQLException;
+}
